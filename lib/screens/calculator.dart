51d972670d660455bf15f69d1932bd73f629e445
Carlos Roberto Barbosa Júnior <carlosjr.if@gmail.com>
2020-02-27 14:41:18 -0300

    [ADD] Adicionando o layout do teclado

--- lib/screens/calculator.dart
+++ lib/screens/calculator.dart
@@ -1,14 +1,19 @@
 import 'package:calculator/components/display.dart';
+import 'package:calculator/components/keyboard.dart';
 import 'package:flutter/material.dart';
 
 class Calculator extends StatelessWidget {
+  _onPressed(String text) {
+    print(text);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Column(
         children: <Widget>[
           Display('123.45'),
-          Text('Teclado'),
+          Keyboard(_onPressed),
         ],
       ),
     );
